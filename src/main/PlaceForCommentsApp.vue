c4546de68aab6e63cc002f9f6db2719a38b3314d
Roman Krutarchuk <makarenaclubg@gmail.com>
2023-03-09 19:12:43 +0200

    init

---
+++ src/main/PlaceForCommentsApp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>main</h1>
+    <FormComponent/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import FormComponent from '@/components/FormComponent.vue'
+import axios from "axios"
+
+export default {
+  name: 'HomeView',
+  components: {
+    FormComponent
+  }
+}
+</script>
