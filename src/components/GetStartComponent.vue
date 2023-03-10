3d21450d0637dd15de59a6fd5edaf183250ffa3e
Roman Krutarchuk <makarenaclubg@gmail.com>
2023-03-10 11:12:21 +0200

    111

--- src/components/GetStartComponent.vue
+++ src/components/GetStartComponent.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <h1>hello you need to create a user, or login</h1>
-    <router-link to="/create"/>
-    <router-link to="/auth"/>
+    <h1> 111 </h1>
+    <!-- <router-link to="/create"/>
+    <router-link to="/auth"/> -->
   </div>
 </template>
 
