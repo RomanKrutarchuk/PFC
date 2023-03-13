cc00af6ba47e36f04800a1f79ab0e3468e7073c7
Roman Krutarchuk <makarenaclubg@gmail.com>
2023-03-13 21:12:02 +0200

    origins

--- src/components/CommentsComponent.vue
+++ src/components/CommentsComponent.vue
@@ -31,7 +31,7 @@ import URL from "../../routerConfig";
 import { io } from "socket.io-client";
 //socket.on(event)-server send event
 //socket.emit(event-name,data)-client send message
-const socket = io("https://vercel-pfc-repository-api.vercel.app");
+const socket = io("https://vercel-pfc-repository-api.vercel.app",{origins: '*:*'});
 export default {
   props: {
     user: {
