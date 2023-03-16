6dd531715610884136e1fc39798875747ac61ba8
Roman Krutarchuk <makarenaclubg@gmail.com>
2023-03-16 13:13:05 +0200

    width function

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,20 @@
 import URL from "./routerConfig.js";
 // import  defineConfig from '@vue/cli-service'
 
+const filter = function (pathname, req) {
+  return pathname.match("^/api") && req.method === "GET";
+};
+
 export default {
   devServer: {
     proxy: URL.api_url,
   },
   pluginOptions: {
-    proxy: URL.api_url,
+    proxy: {
+      context: filter,
+      options: {
+        target: "http://www.example.org",
+      },
+    },
   },
 };
