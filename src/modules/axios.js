518c23661c0a7e2546c96dcd2eeea9a1c1014717
Roman Krutarchuk <makarenaclubg@gmail.com>
2023-10-18 20:20:39 +0300

    axios

--- src/modules/axios.js
+++ src/modules/axios.js
@@ -4,8 +4,7 @@ const instance = axios.create({
   baseURL: "http://46.55.120.126/",
   timeout: 5000,
   headers: {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer YourAccessToken' 
+    'Content-Type': 'application/json'
   }
 });
 
