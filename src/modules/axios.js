32dead54d9d0c048cfe87423936fc705ae1c99da
Roman Krutarchuk <makarenaclubg@gmail.com>
2023-10-18 20:20:17 +0300

    axios

--- src/modules/axios.js
+++ src/modules/axios.js
@@ -2,6 +2,11 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: "http://46.55.120.126/",
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer YourAccessToken' 
+  }
 });
 
 export default instance;
